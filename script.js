3a8afae9f24dba969bbbab5b82d0c73117f3591d
thomas <thomas.zeryouh@goons.fr>
2025-03-11 11:44:57 +0100

    ajout carte en objet et localstorage

--- script.js
+++ script.js
@@ -1,6 +1,68 @@
 console.log("Kanban JS loaded...");
 
 //
+
+
+function creerCarte(titre, contenu, priorite){
+ if(!titre || !contenu) return null;
+
+    const prioritesValides = ['haute', 'moyenne', 'basse'];
+    const prioriteFinale = prioritesValides.includes(priorite) ? priorite : 'basse';
+
+    const carte = {
+        id : Date.now(),
+        titre : titre,
+        contenu : contenu,
+        priorite : prioriteFinale,
+        statut : 'todo'
+    }
+
+    sauvegarderCarteDansLocalStorage(carte)
+    ajouterCarteTableau(carte)
+return carte
+}
+
+function sauvegarderCarteDansLocalStorage(carte) {
+ let cartes = JSON.parse(localStorage.getItem("cartes")) || [];
+ cartes.push(carte);
+ localStorage.setItem("cartes", JSON.stringify(cartes))
+}
+
+ //Creer la carte 
+ function ajouterCarteTableau(carte){
+    const colonneAFaire = document.querySelector('.column[data-status="todo"]');
+    const nouvelleCarte = document.createElement('div');
+    nouvelleCarte.setAttribute('id', carte.id);
+    nouvelleCarte.classList.add('card');
+    nouvelleCarte.setAttribute('data-priority', carte.priorite);
+
+    const nouveauTitre = document.createElement('h3');
+    nouveauTitre.textContent = carte.titre;
+
+    const nouveauContenu = document.createElement('p');
+    nouveauContenu.textContent = carte.contenu;
+
+    const boutonSupprimer = document.createElement('button');
+    boutonSupprimer.textContent = 'Supprimer';
+    boutonSupprimer.addEventListener('click', function () {
+        supprimerCarte(carte.id)
+        nouvelleCarte.remove();
+    });
+
+    nouvelleCarte.appendChild(nouveauTitre);
+    nouvelleCarte.appendChild(nouveauContenu);
+    nouvelleCarte.appendChild(boutonSupprimer);
+
+    colonneAFaire.appendChild(nouvelleCarte);
+
+}
+
+function supprimerCarte(id) {
+    let cartes = JSON.parse(localStorage.getItem("cartes")) || [];
+ cartes = cartes.filter(carte => carte.id !== id);
+ localStorage.setItem("cartes", JSON.stringify(cartes)) 
+}
+
 document.getElementById('addCardBtn').addEventListener('click', function() {
 
     //Récupérer les infos 
@@ -11,35 +73,23 @@ document.getElementById('addCardBtn').addEventListener('click', function() {
     if (!contenu) return;
 
     const priorite = prompt("Priorité de la carte (haute, moyenne, basse) :", "basse");
-    const prioritesValides = ['haute', 'moyenne', 'basse'];
-    const prioriteFinale = prioritesValides.includes(priorite) ? priorite : 'basse';
+   
 
-    const colonneAFaire = document.querySelector('.column[data-status="todo"]');
+    
 
-    //Creer la carte 
-
-    const nouvelleCarte = document.createElement('div');
-    nouvelleCarte.classList.add('card');
-    nouvelleCarte.setAttribute('data-priority', prioriteFinale);
-
-    const nouveauTitre = document.createElement('h3');
-    nouveauTitre.textContent = titre;
-
-    const nouveauContenu = document.createElement('p');
-    nouveauContenu.textContent = contenu;
-
-    const boutonSupprimer = document.createElement('button');
-    boutonSupprimer.textContent = 'Supprimer';
-    boutonSupprimer.addEventListener('click', function() {
-        nouvelleCarte.remove();
-    });
-
-    nouvelleCarte.appendChild(nouveauTitre);
-    nouvelleCarte.appendChild(nouveauContenu);
-    nouvelleCarte.appendChild(boutonSupprimer);
-
-    colonneAFaire.appendChild(nouvelleCarte);
+    creerCarte(titre, contenu, priorite)
+    
 });
+  
+
+ 
+// Charger les cartes depuis le local storage 
+
+window.addEventListener("load", function(){
+    let cartes = JSON.parse(localStorage.getItem("cartes")) || [];
+    cartes.forEach(ajouterCarteTableau);
+})
+
 
 
 
